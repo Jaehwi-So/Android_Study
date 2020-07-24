471c699658c508fc3893a3cec5f764a8c7515e98
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-07-25 00:11:43 +0900

    버튼 효과

---
+++ 12_Button_Effect/app/src/main/java/com/sjh/a12_button_effect/ButtonEffectActivity.java
@@ -0,0 +1,14 @@
+package com.sjh.a12_button_effect;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ButtonEffectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_effect);
+    }
+}
